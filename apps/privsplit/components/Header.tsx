f57c9a81e3bc363657c13f8b673ba6ecf45e7775
Reddawn <35641620+forumevi@users.noreply.github.com>
2025-10-24 21:58:40 +0300

    Update Header.tsx

--- apps/privsplit/components/Header.tsx
+++ apps/privsplit/components/Header.tsx
@@ -1,14 +1,34 @@
 "use client";
-import WalletConnect from "./WalletConnect";
 
 export default function Header() {
   return (
-    <header style={{display:"flex", justifyContent:"space-between", alignItems:"center", padding:20, borderBottom:"1px solid #eee"}}>
-      <div style={{display:"flex", alignItems:"center", gap:12}}>
-        <div style={{width:40, height:40, borderRadius:8, background:"#0070f3", color:"#fff", display:"flex", alignItems:"center", justifyContent:"center"}}>F</div>
-        <div style={{fontWeight:700}}>fhevm-universal</div>
+    <header
+      style={{
+        display: "flex",
+        justifyContent: "flex-start",
+        alignItems: "center",
+        padding: 20,
+        borderBottom: "1px solid #eee",
+      }}
+    >
+      <div style={{ display: "flex", alignItems: "center", gap: 12 }}>
+        <div
+          style={{
+            width: 40,
+            height: 40,
+            borderRadius: 8,
+            background: "#0070f3",
+            color: "#fff",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            fontWeight: 600,
+          }}
+        >
+          F
+        </div>
+        <div style={{ fontWeight: 700 }}>fhevm-universal</div>
       </div>
-      <WalletConnect />
     </header>
   );
 }
